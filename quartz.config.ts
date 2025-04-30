553d44667f6cb74b219a7fcf97e63154f0c288f9
zengls <3186428803@qq.com>
2025-04-30 19:36:26 +0800

    Quartz sync: Apr 30, 2025, 7:36 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Blogs",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "zengls3186428803.github.io/quartz",
+    baseUrl: "zengls3186428803.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
