ed999ea12c5f75b401b702c21f3d0cee8a665f10
zengls <3186428803@qq.com>
2025-04-30 19:24:05 +0800

    Quartz sync: Apr 30, 2025, 7:24 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "zengls3186428803.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
